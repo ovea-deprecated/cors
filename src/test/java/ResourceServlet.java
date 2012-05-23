16db118d440dfb07cfc590d5ddd2101cf45b65d4
mathieucarbou <mathieu.carbou@gmail.com>
2012-05-23 13:51:19 -0400

    improving test page

--- src/test/java/ResourceServlet.java
+++ src/test/java/ResourceServlet.java
@@ -4,13 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.JarURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.*;
 
 /**
  * @author Mathieu Carbou (mathieu.carbou@gmail.com)
@@ -21,16 +15,12 @@ public final class ResourceServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String res = req.getPathInfo().startsWith("/") ? req.getPathInfo().substring(1) : req.getPathInfo();
-        URL url = Thread.currentThread().getContextClassLoader().getResource(res);
-        if (url == null) {
+        File file = new File(res);
+        if (!file.exists()) {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND, req.getPathInfo());
         } else {
-            URLConnection connection = url.openConnection();
-            if (connection instanceof JarURLConnection) {
-                connection.setUseCaches(false);
-            }
             OutputStream out = resp.getOutputStream();
-            try (InputStream in = new BufferedInputStream(connection.getInputStream())) {
+            try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
                 byte[] data = new byte[8096];
                 int c;
                 int len = 0;
