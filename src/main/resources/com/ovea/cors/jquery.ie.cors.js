a441daf8afe40df5929eeeedc376d69d018b57ec
mathieucarbou <mathieu.carbou@gmail.com>
2012-12-14 12:32:05 -0500

    reverted path for issue #6 since it creates a big drawback issue #8

--- src/main/resources/com/ovea/cors/jquery.ie.cors.js
+++ src/main/resources/com/ovea/cors/jquery.ie.cors.js
@@ -40,7 +40,7 @@
  *
  */
 (function ($) {
-    var _this;
+
     if (!('__jquery_xdomain__' in $)
         && $.browser.msie // must be IE
         && 'XDomainRequest' in window // and support XDomainRequest (IE8+)
@@ -225,10 +225,6 @@
                     document.cookie = cooks[i] + ";Domain=" + document.domain;
                 }
                 _done(ReadyState.DONE, resp[0]);
-
-                if(typeof(_this.success) === "function"){
-                    _this.success(resp[1]);
-                }
                 resp = null;
             };
             this.readyState = ReadyState.UNSENT;
@@ -299,7 +295,6 @@
 
         $.ajaxSettings.xhr = function () {
             var target = parseUrl(this.url).domain;
-            _this = this;
             if (target === "" || target === domain) {
                 return oldxhr.call($.ajaxSettings);
             } else {
